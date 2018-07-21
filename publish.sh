cbf5daa3b8444a32bcad83d7dec74a6bb0719ff9
Sridhar Ratnakumar <srid@srid.ca>
2018-07-21 16:52:27 -0400

    Run using entr

--- publish.sh
+++ publish.sh
@@ -6,6 +6,4 @@ set -xe
 
 nix-build -A exe | cachix push srid
 
-ssh srid@tinix.srid.ca 'cd ~/run/revue; and git pull; and nix-build -A exe'
-
-# TODO: Restart the server
+ssh srid@tinix.srid.ca 'cd ~/run/revue; and git pull; and nix-build -A exe; and touch .entr'
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -xe
+
+# Run the app; and restart the server when 'result' symlink gets updated.
+export PORT=9005
+
+touch .entr
+ls result | entr -d sh -c 'cd result; ./backend -p $PORT'
