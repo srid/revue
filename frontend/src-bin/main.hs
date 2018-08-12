8785cca90449744660dd196c013cdd83bddedca3
Sridhar Ratnakumar <srid@srid.ca>
2018-08-12 19:59:34 -0400

    Update obelisk

--- frontend/src-bin/main.hs
+++ frontend/src-bin/main.hs
@@ -1,9 +1,10 @@
-import Common.Route
-import Frontend
 import Obelisk.Frontend
 import Obelisk.Route.Frontend
 import Reflex.Dom
 
+import Common.Route
+
+import Frontend
 main :: IO ()
 main = do
   let Right validFullEncoder = checkEncoder backendRouteEncoder
